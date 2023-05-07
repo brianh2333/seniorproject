f5a3b84e274e7a53b2de3cfdd08a2e4cf6ac6bec
BrianH <hb23619@gmail.com>
2023-05-07 18:16:15 -0400

    fixes

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -120,7 +120,7 @@ public class HealthSystem : MonoBehaviour
 
     IEnumerator Invulnerable() {
         if (isInvulnerable) {
-            yield return new WaitForSeconds(.2f);
+            yield return new WaitForSeconds(.5f);
             isInvulnerable = false;
         }
         yield return null;
